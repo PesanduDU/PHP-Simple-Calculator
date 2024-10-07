a664f0b428dcde45f14ccaa1b1b8a26b14aa3b81
PesanduDU <pesandudubeysekara@gmail.com>
2024-10-07 22:26:01 +0530

    3rd commit

--- index.php
+++ index.php
@@ -4,59 +4,53 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Calculator</title>
 
     <link rel="stylesheet" href="style.css">
-
 </head>
 
 <body>
-
-    <?php
-
-    if ($_SERVER['REQUEST_METHOD'] == "POST"){
-        $input = "";
-
-
-
-
-
-    }
-
-
-    ?>
-
     <form action="<?php echo htmlspecialchars($_SERVER['PHP_SELF']); ?>" method="post">
 
         <div class="calculator">
             <h3 style="color: #7f7979;">Calculator : V.0.1</h3>
 
             <div class="output">
-                <!-- <span class="result" name="expression">1234</span> -->
-                 <!-- <input type="text" name="expression"  class="result"> -->
-                <textarea name="expression" id="" class="result">1234</textarea>
+                <input type="text" class="result" name="expression" value="<?php echo isset($expression) ? $expression : ''; ?>" readonly />
             </div>
             <div class="buttons">
-                <button name="clicked" value="1" onclick="">1</button>
-                <button name="clicked" value="" onclick="">2</button>
-                <button name="clicked" value="" onclick="">3</button>
-                <button name="clicked" value="" onclick="">+</button>
-                <button name="clicked" value="" onclick="">4</button>
-                <button name="clicked" value="" onclick="">5</button>
-                <button name="clicked" value="" onclick="">6</button>
-                <button name="clicked" value="" onclick="">-</button>
-                <button name="clicked" value="" onclick="">7</button>
-                <button name="clicked" value="" onclick="">8</button>
-                <button name="clicked" value="" onclick="">9</button>
-                <button name="clicked" value="" onclick="">*</button>
-                <button class="bg-red" value="" onclick="">C</button>
-                <button name="clicked" value="" onclick="">0</button>
-                <button type="submit" name="equal" class="bg-green">=</button>
-                <button name="clicked" value="" onclick="">/</button>
+                <button type="button" onclick="addVal('1')">1</button>
+                <button type="button" onclick="addVal('2')">2</button>
+                <button type="button" onclick="addVal('3')">3</button>
+                <button type="button" onclick="addVal('+')">+</button>
+                <button type="button" onclick="addVal('4')">4</button>
+                <button type="button" onclick="addVal('5')">5</button>
+                <button type="button" onclick="addVal('6')">6</button>
+                <button type="button" onclick="addVal('-')">-</button>
+                <button type="button" onclick="addVal('7')">7</button>
+                <button type="button" onclick="addVal('8')">8</button>
+                <button type="button" onclick="addVal('9')">9</button>
+                <button type="button" onclick="addVal('*')">*</button>
+                <button type="button" class="bg-red" onclick="clearVal()">C</button>
+                <button type="button" onclick="addVal('0')">0</button>
+                <button type="button" name="equal" class="bg-green">=</button>
+                <button type="button" onclick="addVal('/')">/</button>
             </div>
         </div>
 
     </form>
+
+    <script>
+        function addVal(value) {
+            const resultField = document.querySelector('.result');
+            resultField.value += value;
+        }
+
+        function clearVal() {
+            const resultField = document.querySelector('.result');
+            resultField.value = '';
+        }
+    </script>
 </body>
 
 </html>
